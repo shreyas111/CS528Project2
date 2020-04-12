76d4a237c64b4575c01d7de3f5c9f6954c3a9246
Shreyas Kulkani <shreyaskulkarnilogin@gmail.com>
2020-04-11 19:34:49 -0500

    Added Assets for Sound Placement Scene

--- Assets/Scripts/SoundPlacement/MenuManager.cs
+++ Assets/Scripts/SoundPlacement/MenuManager.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
     public GameObject canvasMenu;
-    public GameObject soundCubeMenu;
+    public GameObject soundCubeMenu;    
     public string activeMenuName;
 
     private void Awake()
@@ -23,7 +24,7 @@ public class MenuManager : MonoBehaviour
                 Debug.Log("MenuName is" + menuName);
                 if(activeMenuName.Equals("SoundCubeMenu"))
                 {
-                    //canvasMenu.SetActive(false);
+                    
                     soundCubeMenu.SetActive(false);
                     activeMenuName = "";
                 }
@@ -31,6 +32,8 @@ public class MenuManager : MonoBehaviour
                 {
                     canvasMenu.SetActive(false);
                     soundCubeMenu.SetActive(true);
+                    setMenuValues("SoundCubeMenu");
+
                     activeMenuName = "SoundCubeMenu";
                 }
                 break;
@@ -52,5 +55,59 @@ public class MenuManager : MonoBehaviour
 
         }
     }
-
+    public void setMenuValues(string menuName)
+    {
+        switch (menuName)
+        {
+            case "SoundCubeMenu":
+                AudioSource soundCube = GameObject.Find("SoundCube").GetComponent<AudioSource>();
+                foreach (Transform trans in soundCubeMenu.GetComponentInChildren<Transform>())
+                {
+                    if (trans.name == "SliderVolume")
+                    {
+                        trans.gameObject.GetComponent<Slider>().value = soundCube.volume;
+                    }
+                    if (trans.name == "SliderPitch")
+                    {
+                        trans.gameObject.GetComponent<Slider>().value = soundCube.pitch;
+                    }
+                    if (trans.name == "SliderPriority")
+                    {
+                        trans.gameObject.GetComponent<Slider>().value = soundCube.priority;
+                    }
+                    if (trans.name == "SliderStereoPan")
+                    {
+                        trans.gameObject.GetComponent<Slider>().value = soundCube.panStereo;
+                    }
+                    if (trans.name == "SliderSpatialBlend")
+                    {
+                        trans.gameObject.GetComponent<Slider>().value = soundCube.spatialBlend;
+                    }
+                    if (trans.name == "SliderReverbMix")
+                    {
+                        trans.gameObject.GetComponent<Slider>().value = soundCube.reverbZoneMix;
+                    }
+                    if (trans.name == "SliderDopplerLevel")
+                    {
+                        trans.gameObject.GetComponent<Slider>().value = soundCube.dopplerLevel;
+                    }
+                    if (trans.name == "SliderSpread")
+                    {
+                        trans.gameObject.GetComponent<Slider>().value = soundCube.spread;
+                    }
+                    if (trans.name == "SliderMinDistance")
+                    {
+                        trans.gameObject.GetComponent<Slider>().value = soundCube.minDistance;
+                    }
+                    if (trans.name == "SliderMaxDistance")
+                    {
+                        trans.gameObject.GetComponent<Slider>().value = soundCube.maxDistance;
+                    }
+                }
+                        
+                break;
+            case "CanvasMenu":
+                break;
+        }
+    }
 }
--- Assets/Scripts/SoundPlacement/SoundCubeMenuInteractions.cs
+++ Assets/Scripts/SoundPlacement/SoundCubeMenuInteractions.cs
@@ -1,17 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundCubeMenuInteractions : MonoBehaviour
 {
 
     AudioSource source;
-    bool isLooped;
+    bool isLooping;
+    public GameObject soundCubeMenu;
     // Start is called before the first frame update
     void Awake()
     {
         source= this.GetComponent<AudioSource>();
-        isLooped = source.loop;
+        isLooping = source.loop;
         
     }
 
@@ -28,8 +31,6 @@ public class SoundCubeMenuInteractions : MonoBehaviour
     }
     public void playPause()
     {
-        Debug.Log("Inside Play Pause");
-
         if(source.isPlaying)
         {
             source.Pause();
@@ -38,12 +39,121 @@ public class SoundCubeMenuInteractions : MonoBehaviour
         {
             source.Play();
         }
-
-        
-
+       
     }
     public void loopUnloop()
     {
-        source.loop = isLooped;
+        isLooping = !isLooping;
+        source.loop = isLooping;
+    }
+    public void stop()
+    {
+        if (source.isPlaying)
+        {
+            source.Stop();
+        }
+    }
+    public void UpdateVolume()
+    {
+        foreach (Transform trans in soundCubeMenu.GetComponentInChildren<Transform>())
+        {
+            if (trans.name == "SliderVolume")
+            {
+                source.volume=trans.gameObject.GetComponent<Slider>().value;
+            }
+        }
+    }
+    public void updatePitch()
+    {
+        foreach (Transform trans in soundCubeMenu.GetComponentInChildren<Transform>())
+        {
+            if (trans.name == "SliderPitch")
+            {
+                source.pitch = trans.gameObject.GetComponent<Slider>().value;
+            }
+        }
+    }
+    public void updatePriority()
+    {
+        foreach (Transform trans in soundCubeMenu.GetComponentInChildren<Transform>())
+        {
+            if (trans.name == "SliderPitch")
+            {
+                source.priority = Convert.ToInt32(trans.gameObject.GetComponent<Slider>().value);
+            }
+        }
+    }
+    public void updateStereoPan()
+    {
+        foreach (Transform trans in soundCubeMenu.GetComponentInChildren<Transform>())
+        {
+            if (trans.name == "SliderStereoPan")
+            {
+                source.panStereo = trans.gameObject.GetComponent<Slider>().value;
+            }
+        }
+
+    }
+    public void updateSpatialBlend()
+    {
+        foreach (Transform trans in soundCubeMenu.GetComponentInChildren<Transform>())
+        {
+            if (trans.name == "SliderSpatialBlend")
+            {
+                source.spatialBlend = trans.gameObject.GetComponent<Slider>().value;
+            }
+        }
+    }
+    public void updateReverbMix()
+    {
+        foreach (Transform trans in soundCubeMenu.GetComponentInChildren<Transform>())
+        {
+            if (trans.name == "SliderReverbMix")
+            {
+                source.reverbZoneMix = trans.gameObject.GetComponent<Slider>().value;
+            }
+        }
+    }
+    public void updateDoplerLevel()
+    {
+        foreach (Transform trans in soundCubeMenu.GetComponentInChildren<Transform>())
+        {
+            if (trans.name == "SliderDopplerLevel")
+            {
+                source.dopplerLevel = trans.gameObject.GetComponent<Slider>().value;
+            }
+        }
+    }
+    public void updateSpread()
+    {
+        foreach (Transform trans in soundCubeMenu.GetComponentInChildren<Transform>())
+        {
+            if (trans.name == "SliderSpread")
+            {
+                source.spread = trans.gameObject.GetComponent<Slider>().value;
+            }
+        }
+    }
+    public void updateMinDistance()
+    {
+        foreach (Transform trans in soundCubeMenu.GetComponentInChildren<Transform>())
+        {
+            if (trans.name == "SliderMinDistance")
+            {
+                source.minDistance = Convert.ToInt32(trans.gameObject.GetComponent<Slider>().value);
+            }
+        }
+
+    }
+    public void updateMaxDistance()
+    {
+        foreach (Transform trans in soundCubeMenu.GetComponentInChildren<Transform>())
+        {
+            //Debug.Log("Distance is Max" + trans.gameObject.GetComponent<Slider>().value);
+            if (trans.name == "SliderMaxDistance")
+            {
+                source.maxDistance = Convert.ToInt32(trans.gameObject.GetComponent<Slider>().value);
+            }
+        }
     }
 }
