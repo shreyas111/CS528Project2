2eee5ad3ac3992563980214877a9dbc79ce707d9
Shreyas Kulkani <shreyaskulkarnilogin@gmail.com>
2020-04-22 03:09:31 -0500

    Added Instruction Panel and Sounds in Sound Placement, And Spacemen in sound shape
    Added Instruction Panel and Sounds in Sound Placement, And Spacemen in sound shape

---
+++ Assets/Scripts/SoundPlacement/InformationTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class InformationTrigger : MonoBehaviour
+{
+    public GameObject informationCanvas;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Enter object" + other.name);
+        if(other.name=="Head")
+        informationCanvas.SetActive(true);
+    }
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exit Object" + other.name);
+        if (other.name == "Head")
+            informationCanvas.SetActive(false);
+    }
+
+
+}
