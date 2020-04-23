12a017e1126e113fc68e86f268d5514399ec0cf7
Shreyas Kulkani <shreyaskulkarnilogin@gmail.com>
2020-04-23 02:00:38 -0500

    Added Sounds and robots area

---
+++ Assets/Scripts/SoundPlacement/DrumInteractions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrumInteractions : MonoBehaviour
+{
+
+    public void PlayDrum()
+    {
+        Debug.Log("Play Drum");
+    }
+}
--- Assets/Scripts/SoundPlacement/InformationTrigger.cs
+++ Assets/Scripts/SoundPlacement/InformationTrigger.cs
@@ -9,13 +9,13 @@ public class InformationTrigger : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Enter object" + other.name);
+      
         if(other.name=="Head")
         informationCanvas.SetActive(true);
     }
     void OnTriggerExit(Collider other)
     {
-        Debug.Log("Exit Object" + other.name);
+        
         if (other.name == "Head")
             informationCanvas.SetActive(false);
     }
